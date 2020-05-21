04abe768097e19e770c6223791e99f8e656d1122
Vinay Pratyush <65728746+Idonthaveausername-tech@users.noreply.github.com>
2020-05-22 01:15:03 +0530

    Create assignment1q5.py

---
+++ day1/assignment1q5.py
@@ -0,0 +1,15 @@
+a=input("Runs scored by Player 1 = ")
+b=input("Runs scored by Player 2 = ")
+c=input("Runs scored by Player 3 = ")
+d=float(a)/60*100
+e=float(b)/60*100
+f=float(c)/60*100
+print("Strike rate of Player 1 = ",d)
+print("Strike rate of Player 2 =",e)
+print("Strike rate of Player 3 = ",f)
+g=d*2
+h=e*2
+i=f*2
+print("Runs scored if Player 1 had played 60 more balls = ",g)
+print("Runs scored if Player 2 had played 60 more balls = ",h)
+print("Runs scored if Player 3 had played 60 more balls = ",i)
